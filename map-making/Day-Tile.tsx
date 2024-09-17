3fde1a09deae92cc1fad859d86e5110d4a686fa9
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-09-17 16:40:02 +0700

    asset: change asset for sprites, add file for tiled application

---
+++ map-making/Day-Tile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="PC Computer - Omori - Faraway Town Exterior Day" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../src/assets/Sprites/PC Computer - Omori - Faraway Town Exterior Day.png" width="1024" height="1024"/>
+</tileset>
