57fc8bbcbdf88172d9059e24026f83036c3b058f
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-09-18 10:47:44 +0700

    feat: map making progress almost finish (not add stop tiles)

---
+++ map-making/Day-BuildingTIle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="PC Computer - Omori - Faraway Town Buildings Day" tilewidth="32" tileheight="32" tilecount="1620" columns="30">
+ <image source="../src/assets/Sprites/PC Computer - Omori - Faraway Town Buildings Day.png" width="960" height="1728"/>
+</tileset>
