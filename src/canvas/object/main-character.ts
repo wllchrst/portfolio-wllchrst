8c70bba9907a4180c99f7b129cbf4078af3b2713
jsmineau <jasmineaudreey@gmail.com>
2024-09-21 18:39:47 +0700

    feat: main character update logic clean from scene to main character class

--- src/canvas/object/main-character.ts
+++ src/canvas/object/main-character.ts
@@ -1,9 +1,41 @@
 import Character from "../parent/character";
+import Setting from "../tools/setting";
 
 export default class MainCharacter extends Character {
-  constructor() {
+  inputs: Map<string, boolean>;
+  character: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+
+  constructor(character: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody) {
     super();
+
+    console.log(`FROM MAIN CHARACTER ${this.main.inputs}`);
+
+    this.inputs = this.main.inputs;
+    this.character = character;
   }
 
-  preload() {}
+  update() {
+    this.movement();
+  }
+
+  // control movement of the main character based on the input.
+  movement() {
+    if (this.inputs == undefined) return;
+    const left = this.inputs.has("a") ? this.inputs.get("a") : false;
+    const right = this.inputs.has("d") ? this.inputs.get("d") : false;
+    const up = this.inputs.has("w") ? this.inputs.get("w") : false;
+    const down = this.inputs.has("s") ? this.inputs.get("s") : false;
+
+    const power = Setting.movePower;
+
+    if (left == true) {
+      this.character.x -= power;
+    } else if (right == true) {
+      this.character.x += power;
+    } else if (down == true) {
+      this.character.y += power;
+    } else if (up == true) {
+      this.character.y -= power;
+    }
+  }
 }
