35d22ff4721eaf53a42e973e3ed3a81ccc599911
wllchrst <william.christian001@binus.ac.id>
2024-09-21 22:07:24 +0700

    feat: add character and movement

--- src/canvas/scene.ts
+++ src/canvas/scene.ts
@@ -1,9 +1,15 @@
 export default class Scene extends Phaser.Scene {
+  aubrey: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody | null = null;
+  cursors: Phaser.Types.Input.Keyboard.CursorKeys | null = null;
   constructor() {
     super();
   }
 
   preload() {
+    this.load.image(
+      "aubrey",
+      "../../public/Sprites/aubrey/walking/forward/0.png"
+    );
     this.load.image(
       "PC Computer - Omori - Faraway Town Buildings Day",
       "../../public/Sprites/PC Computer - Omori - Faraway Town Exterior Day.png"
@@ -45,7 +51,23 @@ export default class Scene extends Phaser.Scene {
     for (const name of layerNames) {
       map.createLayer(name, tileset);
     }
+
+    this.aubrey = this.physics.add.sprite(200, 200, "aubrey");
+
+    this.cameras.main.setBounds(0, 0, 2560, 1280);
+    this.cameras.main.startFollow(this.aubrey);
+    this.cameras.main.setZoom(1.5);
+
+    if (this.input.keyboard == null) return;
+    // Create key objects for the arrow keys
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
-  update() {}
+  update() {
+    if (this.aubrey == null) return;
+    else if (this.cursors == null) return;
+
+    this.aubrey.x += 5;
+    console.log("here");
+  }
 }
