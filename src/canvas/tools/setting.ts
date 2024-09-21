8573f896a23dae494eff0f824a9f911fd256ec7a
jsmineau <jasmineaudreey@gmail.com>
2024-09-21 18:38:14 +0700

    style: clean the code

--- src/canvas/tools/setting.ts
+++ src/canvas/tools/setting.ts
@@ -1,1 +1,23 @@
-export default class Setting {}
+export default class Setting {
+  // config for the game
+  static createConfig(scene: Phaser.Scene): Phaser.Types.Core.GameConfig {
+    return {
+      type: Phaser.AUTO,
+      width: 2560,
+      height: 1280,
+      scene: scene,
+      fps: {
+        limit: 30,
+      },
+      physics: {
+        default: "arcade", // Enable arcade physics
+        arcade: {
+          gravity: { y: 0, x: 0 }, // Set gravity if needed
+          debug: false, // Optionally enable debug mode
+        },
+      },
+    };
+  }
+
+  static movePower = 4;
+}
